7cffc474682c8b7499401b31fbc169abbd95eaf6
Michael Keyser <Erutan409@users.noreply.github.com>
2017-03-21 22:01:57 -0400

    Update CREATE_ALIAS.sh

--- CREATE_ALIAS.sh
+++ CREATE_ALIAS.sh
@@ -1,3 +1,4 @@
 # awk '{print $1/1000 * 1.8 + 32,"Fahrenheit"}' /sys/class/thermal/thermal_zone0/temp
+# Alias: alias temp='awk '"'"'{print $1/1000 * 1.8 + 32,"Fahrenheit"}'"'"' /sys/class/thermal/thermal_zone0/temp'
 echo "alias temp='awk '\"'\"'{print $1/1000 * 1.8 + 32,\"Fahrenheit\"}'\"'\"' /sys/class/thermal/thermal_zone0/temp'" >> ~/.bash_aliases
 source ~/.bashrc
